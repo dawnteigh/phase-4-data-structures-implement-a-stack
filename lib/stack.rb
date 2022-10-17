237b5b4d3ba2b031611f937b9516a1ece704d638
Donte Ladatto <donte279@gmail.com>
2022-10-17 15:22:37 -0500

    commit tree ae1f6eac4a1adc2bb5486a1826cfe0e39f4c70c4

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,53 @@
-# your code here
+class Stack
+  def initialize(limit = nil)
+    @stack = []
+    @limit = limit
+  end
+
+  def push(v)
+    if @limit && @stack.size >= @limit
+      return raise "Stack Overflow"
+    end
+    @stack.push(v)
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def peek
+    @stack[-1]
+  end
+
+  def size
+    @stack.size
+  end
+
+  def empty?
+    @stack.size == 0 ? true : false
+  end
+
+  def full?
+    if @limit
+      return @stack.size >= @limit ? true : false
+    end
+    false
+  end
+
+  def search(v)
+    if !@stack.include?(v)
+      return -1
+    else
+      i = @stack.size - 1
+      @stack.each{ |va|
+      if va != v
+        i -= 1
+      else
+        return i
+      end
+    }
+    end
+  end
+
+  
+end
